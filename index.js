0751ac5c1827316af131c20ac813effaf552593c
justina177 <justinaenenche@yahoo.com>
2023-09-09 02:07:41 +0100

    Added endpoint to GET request query parameters: slack_name a nd track.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 7000;
 
+app.get('/api', (req, res) => {
+    const { slack_name, track } = req.query;
+
+    if (!slack_name || !track) {
+    
+        return res.status(400).json({ error: 'slack_name and track are required parameters' });
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
