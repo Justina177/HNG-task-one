96106258becea25227f53ff404ee37ddb10ed375
justina177 <justinaenenche@yahoo.com>
2023-09-09 03:24:23 +0100

    Added function to get current UTC time

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ app.get('/api', (req, res) => {
       // Get current day of the week
   const currentDay = new Date().toLocaleDateString('en-US', { weekday: 'long' });
 
-  
+  // Get current UTC time
+  const now = new Date();
+
+  // Extract the date and time components
+    const year = now.getUTCFullYear();
+    const month = String(now.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(now.getUTCDate()).padStart(2, '0');
+    const hours = String(now.getUTCHours()).padStart(2, '0');
+    const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+
+    // Set the UTC time as "yyyy-MM-ddTHH:mm:ssZ"
+    const utcTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
+
+
+
+
 })
 
 
